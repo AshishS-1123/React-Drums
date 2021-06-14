9dbbeddbd60ccf562d62a4fbae132eaca9d2b08f
Ashish Shevale <shevaleashish@gmail.com>
2021-06-14 18:11:38 +0530

    Added json file for data

---
+++ src/audio_data.js
@@ -0,0 +1,51 @@
+const path_prefix = "./resources/"
+
+export const audio_data = [
+    {
+        id: 1,
+        key_name: "Q",
+        audio_path: path_prefix + "clap.wav"
+    },
+    {
+        id: 2,
+        key_name: "W",
+        audio_path: path_prefix + "hihat.wav"
+    },
+    {
+        id: 3,
+        key_name: "E",
+        audio_path: path_prefix + "kick.wav"
+    },
+    {
+        id: 4,
+        key_name: "A",
+        audio_path: path_prefix + "openhat.wav"
+    },
+    {
+        id: 5,
+        key_name: "S",
+        audio_path: path_prefix + "boom.wav"
+    },
+
+    {
+        id: 6,
+        key_name: "D",
+        audio_path: path_prefix + "ride.wav"
+    },
+
+    {
+        id: 7,
+        key_name: "Z",
+        audio_path: path_prefix + "snare.wav"
+    },
+    {
+        id: 8,
+        key_name: "X",
+        audio_path: path_prefix + "tom.wav"
+    },
+    {
+        id: 9,
+        key_name: "C",
+        audio_path: path_prefix + "tink.wav"
+    },
+]
